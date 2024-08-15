8085e1bfff0372727e489851c97f98af4f2dcff8
leopcortes <leo.pereira.cortes@gmail.com>
2024-08-14 23:18:35 -0300

    Componente Forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,98 @@
+import { FormComponent } from "~/components/forms/formsContainer";
+
 export default function Home() {
-  return <h1>STRATEGIS</h1>;
+  return (
+    <div className="flex flex-col gap-[8px] p-[32px]">
+      {/* <h1 className="">STRATEGIS</h1> */}
+      <FormComponent>
+        <FormComponent.Title>Cadastrar Usuário</FormComponent.Title>
+        <FormComponent.Line>
+          <FormComponent.Frame>
+            <FormComponent.Label>Email</FormComponent.Label>
+            <FormComponent.Input
+              placeholder="Endereço de email"
+              type="email"
+            ></FormComponent.Input>
+          </FormComponent.Frame>
+          <FormComponent.Frame>
+            <FormComponent.Label>Senha</FormComponent.Label>
+            <FormComponent.Input
+              placeholder="Crie uma senha para acesso ao sistema"
+              type="password"
+            ></FormComponent.Input>
+          </FormComponent.Frame>
+          <FormComponent.Frame>
+            <FormComponent.Label>Confirmar Senha</FormComponent.Label>
+            <FormComponent.Input
+              placeholder="Confirme a senha"
+              type="password"
+            ></FormComponent.Input>
+          </FormComponent.Frame>
+        </FormComponent.Line>
+
+        <FormComponent.Line>
+          <FormComponent.Frame>
+            <FormComponent.Label>Nome</FormComponent.Label>
+            <FormComponent.Input placeholder="Nome completo"></FormComponent.Input>
+          </FormComponent.Frame>
+          <FormComponent.Frame>
+            <FormComponent.Label>Telefone</FormComponent.Label>
+            <FormComponent.Input
+              placeholder="(XX) XXXXX-XXXX"
+              type="tel"
+            ></FormComponent.Input>
+          </FormComponent.Frame>
+          <FormComponent.Frame>
+            <FormComponent.Label>Empresa</FormComponent.Label>
+            <FormComponent.Select
+              placeholder="Selecione uma empresa"
+              values={[
+                "Empresa 1",
+                "Empresa 2",
+                "Empresa 3",
+                "Empresa 4",
+                "Empresa 5",
+              ]}
+            ></FormComponent.Select>
+          </FormComponent.Frame>
+          <FormComponent.Frame>
+            <FormComponent.Label>Cargo</FormComponent.Label>
+            <FormComponent.Select
+              placeholder="Selecione um cargo"
+              values={[
+                "Administrador",
+                "Operador",
+                "Estoquista",
+                "Requisitante",
+                "Personalizado",
+              ]}
+            ></FormComponent.Select>
+          </FormComponent.Frame>
+        </FormComponent.Line>
+
+        <FormComponent.Line>
+          <FormComponent.Frame>
+            <FormComponent.Label>Empresa</FormComponent.Label>
+            <FormComponent.Input placeholder="Nome da empresa"></FormComponent.Input>
+          </FormComponent.Frame>
+          <FormComponent.Frame>
+            <FormComponent.Label>CNPJ</FormComponent.Label>
+            <FormComponent.Input placeholder="XX.XXX.XXX/XXXX-XX"></FormComponent.Input>
+          </FormComponent.Frame>
+          <FormComponent.Frame>
+            <FormComponent.Label>Tipo de Empresa</FormComponent.Label>
+            <FormComponent.Select
+              placeholder="Selecione um cargo"
+              values={["Matriz", "Filial"]}
+            ></FormComponent.Select>
+          </FormComponent.Frame>
+        </FormComponent.Line>
+        <FormComponent.ButtonLayout>
+          <FormComponent.Button className="bg-[#28A745] hover:bg-[#309147]">
+            Criar Usuário
+          </FormComponent.Button>
+        </FormComponent.ButtonLayout>
+      </FormComponent>
+    </div>
+  );
 }
---
+++ src/components/forms/formsContainer.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+import { Input } from "~/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { cn } from "../../lib/utils";
+
+type FormProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+export const FormComponent = (props: FormProps) => {
+  const rootStyle = cn("flex flex-col gap-[12px]", props.className);
+  return <div className={rootStyle}>{props.children}</div>;
+};
+
+type FormComponentTitleProps = {
+  className?: string;
+  children: string;
+};
+
+FormComponent.Title = function FormComponentTitle(
+  props: FormComponentTitleProps,
+) {
+  const style = cn("flex text-[32px] font-inter font-medium", props.className);
+  return <p className={style}>{props.children}</p>;
+};
+
+type FormComponentLineProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+FormComponent.Line = function FormComponentLine(props: FormComponentLineProps) {
+  const style = cn(
+    "flex sm:flex-row flex-col gap-[6px] sm:gap-[12px] w-full",
+    props.className,
+  );
+  return <div className={style}>{props.children}</div>;
+};
+
+type FormComponentFrameProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+FormComponent.Frame = function FormComponentFrame(
+  props: FormComponentFrameProps,
+) {
+  const style = cn("flex flex-col gap-[0px] w-full", props.className);
+  return <div className={style}>{props.children}</div>;
+};
+
+type FormComponentLabelProps = {
+  className?: string;
+  children: string;
+};
+
+FormComponent.Label = function FormComponentLabel(
+  props: FormComponentLabelProps,
+) {
+  const style = cn("font-inter text-[16px] font-regular", props.className);
+  return <p className={style}>{props.children}</p>;
+};
+
+type FormComponentInputProps = {
+  className?: string;
+  placeholder?: string;
+  type?: string;
+};
+
+FormComponent.Input = function FormComponentInput(
+  props: FormComponentInputProps,
+) {
+  const style = cn(
+    "border-[1px] border-[#DEE2E6] bg-white placeholder:text-[#ADB5BD]",
+    props.className,
+  );
+  return (
+    <Input
+      className={style}
+      placeholder={props.placeholder}
+      type={props.type}
+    ></Input>
+  );
+};
+
+type FormComponentSelectProps = {
+  className?: string;
+  placeholder?: string;
+  values: string[];
+};
+
+FormComponent.Select = function FormComponentSelect(
+  props: FormComponentSelectProps,
+) {
+  const style = cn(
+    "border-[1px] border-[#DEE2E6] bg-white placeholder-[#ADB5BD]",
+    props.className,
+  );
+  return (
+    <Select>
+      <SelectTrigger className={style}>
+        <SelectValue placeholder={props.placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        {props.values.map((value) => (
+          <SelectItem key={value} value={value}>
+            {value}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+type FormComponentButtonLayoutProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+FormComponent.ButtonLayout = function FormComponentButtonLayout(
+  props: FormComponentButtonLayoutProps,
+) {
+  const style = cn("flex w-full gap-3 justify-center pt-1", props.className);
+  return <div className={style}>{props.children}</div>;
+};
+
+type FormComponentButtonProps = {
+  handlePress?: () => void;
+  className?: string;
+  children: string;
+};
+
+FormComponent.Button = function FormComponentButton(
+  props: FormComponentButtonProps,
+) {
+  const style = cn(
+    "bg-red-300 px-[20px] py-[8px] rounded-lg min-w-28",
+    props.className,
+  );
+  return (
+    <button onClick={props.handlePress} className={style}>
+      <p className="text-[12px] font-semibold tracking-wider text-white sm:text-[16px] sm:tracking-normal">
+        {props.children}
+      </p>
+    </button>
+  );
+};
