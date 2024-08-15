96d0ee0892d370cce0a8e689fdcfe70fa67a5421
leopcortes <leo.pereira.cortes@gmail.com>
2024-08-15 15:46:18 -0300

    Componente dos botões das tabelas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { TableButtonComponent } from "~/components/tableButton";
+
 export default function Home() {
-  return <h1>STRATEGIS</h1>;
+  return (
+    <div className="p-8">
+      <h1 className="rounded-">A</h1>
+      <TableButtonComponent className="">
+        <TableButtonComponent.Button className="bg-[#3398F5] hover:bg-[#2A82D7]">
+          Adicionar Nota Fiscal
+        </TableButtonComponent.Button>
+      </TableButtonComponent>
+      <TableButtonComponent className="sm:pt-3">
+        <TableButtonComponent.Button className="bg-[#D01F30] hover:bg-[#B01A29]">
+          Notas Fiscais Aceitas
+        </TableButtonComponent.Button>
+      </TableButtonComponent>
+    </div>
+  );
 }
