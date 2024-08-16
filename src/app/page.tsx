8179bc3b8db60af70a1ab4b86716a6cbe9b4334e
MatheusNevs <matheusnf62@gmail.com>
2024-08-16 18:46:16 -0300

    feat: somethings more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+import { Filter } from "~/components/filter";
+import { BookMarked } from "lucide-react";
+import { useState } from "react";
+
 export default function Home() {
-  return <h1>STRATEGIS</h1>;
+  //exemplo
+  const [select, setSelect] = useState("");
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Filter
+        icon={({ className }) => (
+          <BookMarked className={className}> </BookMarked>
+        )}
+      >
+        <Filter.Select
+          placeholder="Fornecedor"
+          state={select}
+          setState={setSelect}
+        >
+          <Filter.SelectItems value="Primeiro"></Filter.SelectItems>
+          <Filter.SelectItems value="Segundo"></Filter.SelectItems>
+        </Filter.Select>
+      </Filter>
+    </div>
+  );
 }
--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -1,26 +1,43 @@
 import React from "react";
-import { cn } from "../../lib/utils";
+import { cn } from "~/lib/utils";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 
-type FiltersProps = {
+type FilterProps = {
   className?: string;
   children: React.ReactNode;
+  icon: ({ className }: { className: string }) => React.ReactNode;
 };
 
-const Filters = (props: FiltersProps) => {
-  const style = cn("flex w-full h-fit gap-[16px]", props.className);
-  return <div className={style}>{props.children}</div>;
+export const Filter = (props: FilterProps) => {
+  const style = cn(
+    "flex items-center justify-center rounded-[12px] bg-filtro px-[12px] py-[6px] gap-[10px]",
+    props.className,
+  );
+  return (
+    <div className={style}>
+      <props.icon className={"size-[16px] stroke-[1.5px]"}></props.icon>
+      {props.children}
+    </div>
+  );
 };
 
-type FiltersInputProps = {
+type FilterInputProps = {
   className?: string;
   state: string;
   setState: (value: string) => void;
   placeholder: string;
 };
 
-Filters.Input = function FiltersInput(props: FiltersInputProps) {
+Filter.Input = function FilterInput(props: FilterInputProps) {
   const style = cn(
-    "bg-trasnparent font-inter font-regular text-[#000000] opacity-100 placeholder-opacity-50",
+    "bg-trasnparent font-inter  text-[#000000] opacity-100 placeholder-opacity-50",
     props.className,
   );
   return (
@@ -34,13 +51,47 @@ Filters.Input = function FiltersInput(props: FiltersInputProps) {
   );
 };
 
-type FiltersSelectProps = {
+type FilterSelectProps = {
   className?: string;
+  placeholder: string;
   state: string;
   setState: (value: string) => void;
+  children: React.ReactNode[];
 };
 
-Filters.Select = function FiltersSelect(props: FiltersSelectProps) {
-  const style = cn("", props.className);
-  return <div className={style}></div>;
+Filter.Select = function FilterSelect(props: FilterSelectProps) {
+  const style = cn(
+    "w-fit gap-[10px] border-none bg-transparent font-inter text-[20px] font-regular text-[#000000] opacity-100 placeholder:opacity-50",
+    props.className,
+  );
+  return (
+    <Select
+      value={props.state}
+      onValueChange={(value) => props.setState(value)}
+    >
+      <SelectTrigger className={style + "placeholder-opacity-10"}>
+        <SelectValue placeholder={props.placeholder} />
+      </SelectTrigger>
+      <SelectContent className="bg-transparent">
+        <SelectGroup>{props.children}</SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+type FilterSelectItemsProps = {
+  className?: string;
+  value: string;
+};
+
+Filter.SelectItems = function FilterSelectItems(props: FilterSelectItemsProps) {
+  const style = cn(
+    "font-regular font-inter text-[16px] text-[#000000]",
+    props.className,
+  );
+  return (
+    <SelectItem className={style} value={props.value}>
+      {props.value}
+    </SelectItem>
+  );
 };
