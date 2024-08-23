0cb91366d5a7b0bcd03f2a04eee785b1dd710bf4
leopcortes <leo.pereira.cortes@gmail.com>
2024-08-23 00:28:55 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,7 @@
-"use client";
-import { useState } from "react";
-import { MultiSelect } from "~/components/ui/multi-select";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "~/components/ui/select";
-
-const frameworksList = [
-  { value: "react", label: "React" },
-  { value: "angular", label: "Angular" },
-  { value: "vue", label: "Vue" },
-  { value: "svelte", label: "Svelte" },
-  { value: "ember", label: "Ember" },
-];
-
 export default function Home() {
-  const [selectedFrameworks, setSelectedFrameworks] = useState<string[]>([
-    "react",
-    "angular",
-  ]);
   return (
     <div className="p-4 sm:p-8">
       <h1>STRATEGIS</h1>
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="Empresa 1">Empresa 1</SelectItem>
-          <SelectItem value="Empresa 2">Empresa 2</SelectItem>
-          <SelectItem value="Empresa 3">Empresa 3</SelectItem>
-        </SelectContent>
-      </Select>
-      <MultiSelect
-        options={frameworksList}
-        onValueChange={setSelectedFrameworks}
-        defaultValue={selectedFrameworks}
-        placeholder="Select frameworks"
-        variant="inverted"
-        maxCount={3}
-      />
-      <div className="mt-4">
-        <h2 className="text-xl font-semibold">Selected Frameworks:</h2>
-        <ul className="list-inside list-disc">
-          {selectedFrameworks.map((framework) => (
-            <li key={framework}>{framework}</li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 }
