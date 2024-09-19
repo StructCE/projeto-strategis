01968d706983be41f4d8094a633cfaf8dc32095a
leopcortes <leo.pereira.cortes@gmail.com>
2024-09-19 16:35:14 -0300

    botão link para criar novo inventário

--- src/app/GestaoDeEstoque/InventariosDeEstoque/page.tsx
+++ src/app/GestaoDeEstoque/InventariosDeEstoque/page.tsx
@@ -1,3 +1,4 @@
+import { ExternalLink } from "lucide-react";
 import { TableButtonComponent } from "~/components/tableButton";
 import ManageInventoriesTable from "./_components/manageInventories/manageInventories";
 
@@ -9,8 +10,14 @@ export default function StockInventories() {
         <TableButtonComponent.Link
           link_ref="/GestaoDeEstoque/InventariosDeEstoque/CriarInventario"
           className="bg-vermelho_botao_1 hover:bg-hover_vermelho_botao"
+          placeholder="Criar Novo Inventário"
         >
-          Criar Novo Inventário
+          <ExternalLink
+            className="flex h-full cursor-pointer self-center"
+            size={20}
+            strokeWidth={2.2}
+            color="white"
+          />
         </TableButtonComponent.Link>
       </TableButtonComponent>
     </div>
