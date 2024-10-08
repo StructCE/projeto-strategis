85b806dba1419adf439bb69ae930db24d256fc6b
leopcortes <leo.pereira.cortes@gmail.com>
2024-10-07 22:48:04 -0300

    importação e extração de dados dos xmls

--- src/app/GestaoDeEstoque/ImportacaoDeNFs/page.tsx
+++ src/app/GestaoDeEstoque/ImportacaoDeNFs/page.tsx
@@ -6,7 +6,6 @@ import {
   Calendar,
   Eraser,
   ExternalLink,
-  FolderCog,
   Search,
   Truck,
 } from "lucide-react";
@@ -18,6 +17,8 @@ import { TableComponent } from "~/components/table";
 import { TableButtonComponent } from "~/components/tableButton";
 import { Button } from "~/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import {
   Tooltip,
@@ -27,10 +28,9 @@ import {
 } from "~/components/ui/tooltip";
 import { default as InvoiceDetails } from "./_components/invoiceDetails/invoiceDetails";
 import {
-  inputPath,
   type Invoice,
   invoices,
-  outputPath,
+  type Product,
 } from "./_components/invoicesData";
 
 export default function ImportacaoDeNFs() {
@@ -125,42 +125,144 @@ export default function ImportacaoDeNFs() {
       .replace(/\s/g, "")}-[${productsString}]`;
   }
 
+  type ExtractedInvoiceData = {
+    document_number: string | null | undefined;
+    date_document: Date | null | undefined;
+    supplier: string | null | undefined;
+    installment: string | null | undefined;
+    date_deadline: Date | null | undefined;
+    products: Product[] | null | undefined; // caso deseje incluir
+  };
+
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [extractedData, setExtractedData] = useState<ExtractedInvoiceData[]>(
+    [],
+  );
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const xmlFiles = Array.from(files).filter(
+        (file) => file.type === "text/xml" || file.name.endsWith(".xml"),
+      );
+      setSelectedFiles(xmlFiles);
+    }
+  };
+
+  const handleImport = async () => {
+    const data = [];
+
+    for (const file of selectedFiles) {
+      const text = await file.text(); // Lê o conteúdo do arquivo
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(text, "text/xml"); // Analisa o XML
+
+      // Extrai os dados desejados
+      const documentNumber =
+        xmlDoc.getElementsByTagName("cNF")[0]?.textContent ?? null; // Número do documento
+      const dateDocumentStr =
+        xmlDoc.getElementsByTagName("dhEmi")[0]?.textContent; // Data de emissão
+      const dateDocument = dateDocumentStr ? new Date(dateDocumentStr) : null; // Converte para Date
+      const supplierName = xmlDoc.getElementsByTagName("xNome")[0]?.textContent; // Nome do fornecedor
+      const installment =
+        xmlDoc.getElementsByTagName("nDup")[0]?.textContent ?? null; // Número da parcela, se aplicável
+      const dateDeadlineStr =
+        xmlDoc.getElementsByTagName("dVenc")[0]?.textContent; // Data de vencimento da NF
+      const dateDeadline = dateDeadlineStr ? new Date(dateDeadlineStr) : null; // Converte para Date
+
+      // Extrai produtos
+      const products: Product[] = [];
+      const productNodes = xmlDoc.getElementsByTagName("det");
+
+      for (const productNode of productNodes) {
+        const prodNode = productNode.getElementsByTagName("prod")[0];
+        const product: Product = {
+          code: prodNode?.getElementsByTagName("cProd")[0]?.textContent ?? "", // Código do produto
+          name: prodNode?.getElementsByTagName("xProd")[0]?.textContent ?? "", // Descrição do produto
+          purchase_quantity: parseFloat(
+            prodNode?.getElementsByTagName("qCom")[0]?.textContent ?? "0",
+          ), // Quantidade
+          value_unit: parseFloat(
+            prodNode?.getElementsByTagName("vProd")[0]?.textContent ?? "0",
+          ), // Valor unitário
+          ncm: parseFloat(
+            prodNode?.getElementsByTagName("NCM")[0]?.textContent ?? "0",
+          ),
+          cfop: parseFloat(
+            prodNode?.getElementsByTagName("CFOP")[0]?.textContent ?? "0",
+          ),
+          buy_unit: {
+            description:
+              prodNode?.getElementsByTagName("uCom")[0]?.textContent ?? "",
+            abbreviation: "",
+            unitsPerPack: 0,
+          },
+          // Não sei se esses campos são necessários aqui
+          type_of_control: { description: "" },
+          product_category: { description: "" },
+          sector_of_use: { description: "" },
+          address: { stock: "", storage: "", shelf: "" },
+        };
+        products.push(product);
+      }
+
+      data.push({
+        document_number: documentNumber,
+        date_document: dateDocument,
+        supplier: supplierName,
+        installment: installment,
+        date_deadline: dateDeadline,
+        products,
+      });
+    }
+
+    console.log("Todos os dados extraídos:", data); // Criar as Invoices aqui com os dados extraidos
+    setExtractedData(data);
+  };
+
   return (
     <div className="flex w-full flex-col gap-2 bg-fundo_branco text-[16px] font-semibold">
       <TableComponent>
         <TableComponent.Title>Importação de Notas Fiscais</TableComponent.Title>
       </TableComponent>
 
-      <div className="my-2 flex flex-col items-center justify-between sm:my-1 md:flex-row">
-        <div className="flex-col">
-          <button className="flex flex-col items-center gap-2 py-1 sm:flex-row">
-            <div className="flex gap-1">
-              <FolderCog />
-              <p className="font-bold">Origem:</p>
-            </div>
-            <p className="font-normal hover:underline">{inputPath}</p>
-          </button>
-          <button className="flex flex-col items-center gap-2 py-1 sm:flex-row">
-            <div className="flex gap-1">
-              <FolderCog />
-              <p className="font-bold">Destino:</p>
-            </div>
-            <p className="font-normal hover:underline">{outputPath}</p>
-          </button>
+      <div className="my-1 flex flex-col items-center justify-start gap-1.5 sm:my-2 md:flex-row md:justify-between">
+        <div className="flex flex-col gap-1.5">
+          <Label className="font-normal">
+            Selecione arquivos .XML para importá-los
+          </Label>
+          <div className="flex items-center gap-2">
+            <Input
+              type="file"
+              multiple
+              accept=".xml"
+              id="xmlFileInput"
+              onChange={handleFileChange}
+              className="cursor-pointer font-normal hover:bg-[#F6F6F6]"
+            />
+            <Button
+              className="h-fit bg-cinza_escuro_botao px-[20px] py-[7px] hover:bg-cinza_borda_acordeao"
+              onClick={handleImport}
+            >
+              Importar
+            </Button>
+          </div>
         </div>
 
-        <TableButtonComponent.Link
-          link_ref="/GestaoDeEstoque/ImportacaoDeNFs/ImportarNF"
-          className="h-fit bg-azul_botao hover:bg-hover_azul_botao"
-          placeholder="Adicionar Nota Fiscal Manualmente"
-        >
-          <ExternalLink
-            className="flex h-full cursor-pointer self-center"
-            size={20}
-            strokeWidth={2.2}
-            color="white"
-          />
-        </TableButtonComponent.Link>
+        <div className="flex flex-col gap-2">
+          <TableButtonComponent.Link
+            link_ref="/GestaoDeEstoque/ImportacaoDeNFs/ImportarNF"
+            className="h-fit bg-azul_botao hover:bg-hover_azul_botao"
+            placeholder="Adicionar Nota Fiscal Manualmente"
+          >
+            <ExternalLink
+              className="flex h-full cursor-pointer self-center"
+              size={20}
+              strokeWidth={2.2}
+              color="white"
+            />
+          </TableButtonComponent.Link>
+        </div>
       </div>
 
       <Tabs
