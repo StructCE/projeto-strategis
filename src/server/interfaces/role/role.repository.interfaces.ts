eb8c235cf0af8b73669cff4c017d3f466e4517d6
MatheusNevs <matheusnf62@gmail.com>
2024-10-09 19:44:19 -0300

    feat: role procedures

---
+++ src/server/interfaces/role/role.repository.interfaces.ts
@@ -0,0 +1,36 @@
+import z from "zod";
+
+const registerProps = z.object({
+  name: z.string(),
+  modules: z.array(z.number()),
+});
+
+type RegisterProps = z.infer<typeof registerProps>;
+
+const editProps = z.object({
+  id: z.string(),
+  data: z.object({
+    name: z.string().optional(),
+    modules: z.array(z.number()).optional(),
+  }),
+});
+
+type EditProps = z.infer<typeof editProps>;
+
+const deleteProps = z.object({
+  id: z.string(),
+});
+
+type DeleteProps = z.infer<typeof deleteProps>;
+
+export const roleRepositorySchema = {
+  registerProps,
+  editProps,
+  deleteProps,
+};
+
+export type RoleRepositoryInterfaces = {
+  RegisterProps: RegisterProps;
+  EditProps: EditProps;
+  DeleteProps: DeleteProps;
+};
