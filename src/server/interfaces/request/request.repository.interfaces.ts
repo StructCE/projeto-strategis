eb08ab10611cf4311d0c3f459fe4b01c6ce68eb5
MatheusNevs <matheusnf62@gmail.com>
2024-10-14 16:42:37 -0300

    feat: request repository interfaces

---
+++ src/server/interfaces/request/request.repository.interfaces.ts
@@ -0,0 +1,73 @@
+import z from "zod";
+
+const getAllProps = z.object({
+  filters: z.object({
+    date: z.date(),
+    requestResponsible: z.string(),
+    status: z.string(),
+  }),
+});
+
+type GetAllProps = z.infer<typeof getAllProps>;
+
+const registerProps = z.object({
+  description: z.string(),
+  requestDate: z.date(),
+  statusDescription: z.string().optional(),
+  statusDate: z.date().optional(),
+  statusResponsibleId: z.string(),
+  responsibleId: z.string(),
+  status: z.string(),
+  stockId: z.string(),
+  requestProducts: z.array(
+    z.object({
+      requestQuantity: z.number(),
+      releasedQuantity: z.number(),
+      requestId: z.string(),
+      productId: z.string(),
+      unitId: z.string(),
+    }),
+  ),
+});
+
+type RegisterProps = z.infer<typeof registerProps>;
+
+const editProps = z.object({
+  id: z.string(),
+  requestData: z.object({
+    description: z.string().optional(),
+    requestDate: z.date().optional(),
+    statusDescription: z.string().optional().optional(),
+    statusDate: z.date().optional().optional(),
+    statusResponsibleId: z.string().optional(),
+    responsibleId: z.string().optional(),
+    status: z.string().optional(),
+    stockId: z.string().optional(),
+    requestProducts: z.array(
+      z.object({
+        id: z.string(),
+        requestProduct: z.object({
+          requestQuantity: z.number().optional(),
+          releasedQuantity: z.number().optional(),
+          requestId: z.string().optional(),
+          productId: z.string().optional(),
+          unitId: z.string().optional(),
+        }),
+      }),
+    ),
+  }),
+});
+
+type EditProps = z.infer<typeof editProps>;
+
+export const requestRepositorySchema = {
+  getAllProps,
+  registerProps,
+  editProps,
+};
+
+export type RequestRepositoryInterfaces = {
+  GetAllProps: GetAllProps;
+  RegisterProps: RegisterProps;
+  EditProps: EditProps;
+};
