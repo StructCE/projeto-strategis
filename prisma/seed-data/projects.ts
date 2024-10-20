3851dd282fc9e8d94bdbffabc5db9ea9c816c747
leopcortes <leo.pereira.cortes@gmail.com>
2024-10-20 15:19:32 -0300

    schema e seed de invoices

---
+++ prisma/seed-data/projects.ts
@@ -0,0 +1,9 @@
+export const projects: {
+  name: string;
+}[] = [
+  { name: "Strategis - Assessoria" },
+  { name: "Strategis - BPO Financeiro" },
+  { name: "Strategis - Compartilhado" },
+  { name: "Strategis - FidelizClub" },
+  { name: "Strategis - Geral" },
+];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,14 @@
 // CRIAR MODULES, ROLEMODULES AND ROLES
 import { db } from "~/server/db";
+import { account_plans } from "./seed-data/accountPlans";
 import { adjustementReasons } from "./seed-data/adjustmentReasons";
+import { banks } from "./seed-data/banks";
 import { categories } from "./seed-data/categories";
 import { controlTypes } from "./seed-data/controlType";
+import { documentTypes } from "./seed-data/documentTypes";
+import { groups } from "./seed-data/group";
 import { modules } from "./seed-data/modules";
+import { projects } from "./seed-data/projects";
 import { roles } from "./seed-data/roles";
 import { sectorsOfUse } from "./seed-data/sectorOfUse";
 import { units } from "./seed-data/units";
@@ -64,6 +69,34 @@ async function createUnit(props: {
     data: { ...props },
   });
   return createdUnit;
+}
+
+async function createBank(props: { name: string }) {
+  const createdBank = await db.bank.create({
+    data: { ...props },
+  });
+  return createdBank;
+}
+
+async function createProject(props: { name: string }) {
+  const createdProject = await db.project.create({
+    data: { ...props },
+  });
+  return createdProject;
+}
+
+async function createDocumentType(props: { name: string }) {
+  const createdDocumentType = await db.documentType.create({
+    data: { ...props },
+  });
+  return createdDocumentType;
+}
+
+async function createGroup(props: { name: string }) {
+  const createdGroup = await db.group.create({
+    data: { ...props },
+  });
+  return createdGroup;
 }
 
 async function createRoleModules(props: { roleId: string; modules: number[] }) {
@@ -170,52 +203,104 @@ async function main() {
   // });
   // await Promise.all(createdUnits);
 
-  const companyStruct = await db.company.create({
-    data: {
-      name: "Struct EJ",
-      email: "projetostrategis@gmail.com",
-      cnpj: "21.803.569/0001-65",
-      type: "Matriz",
-      phone: "(32) 3025-0102",
-      stateRegistration: "0771508800122",
-      taxRegime: "Simples Nacional",
-      address:
-        "Campus Universitario Darcy Ribeiro S/n Univ de Brasilia Edif Predio SG",
-      city: "Brasília",
-      neighborhood: "Asa Norte",
-      federativeUnit: "DF",
-      cep: "70910-900",
-      // legalResponsibleId: "",
-    },
+  const createdBanks = banks.map(async (bank) => {
+    const createdBank = await createBank({
+      name: bank.name,
+    });
+    return createdBank;
   });
-  await createUserWithRole({
-    name: "Leonardo Côrtes",
-    email: "leonardo.cortes@struct.unb.br",
-    phone: "(61) 99116-4633",
-    roleName: "Administrador",
-    companyId: companyStruct.id,
+  await Promise.all(createdBanks);
+
+  const createdProjects = projects.map(async (project) => {
+    const createdProject = await createProject({
+      name: project.name,
+    });
+    return createdProject;
   });
-  await createUserWithRole({
-    name: "Matheus das Neves Fernandes",
-    email: "matheusnf@struct.unb.br",
-    phone: "(61) 99999-9999",
-    roleName: "Administrador",
-    companyId: companyStruct.id,
+  await Promise.all(createdProjects);
+
+  const createdGroups = groups.map(async (group) => {
+    const createdGroup = await createGroup({
+      name: group.name,
+    });
+    return createdGroup;
   });
-  await createUserWithRole({
-    name: "Guilherme Sampaio",
-    email: "guilherme.sampaio@struct.unb.br",
-    phone: "(61) 99999-9999",
-    roleName: "Administrador",
-    companyId: companyStruct.id,
+  await Promise.all(createdGroups);
+
+  const createdDocumentTypes = documentTypes.map(async (type) => {
+    const createdDocumentType = await createDocumentType({
+      name: type.name,
+    });
+    return createdDocumentType;
   });
-  await createUserWithRole({
-    name: "Willyan Marques",
-    email: "willyan.marques@struct.unb.br",
-    phone: "(61) 99999-9999",
-    roleName: "Administrador",
-    companyId: companyStruct.id,
-  });
+  await Promise.all(createdDocumentTypes);
+
+  for (const plan of account_plans) {
+    // 1. Cria InvoiceAccountPlan
+    const createdPlan = await db.invoiceAccountPlan.create({
+      data: {
+        name: plan.name,
+        abbreviation: plan.abbreviation,
+      },
+    });
+
+    // 2. Para cada conta no plano, cria InvoiceAccount
+    for (const account of plan.accounts) {
+      await db.invoiceAccount.create({
+        data: {
+          name: account.name,
+          accountPlanId: createdPlan.id, // Relaciona com o plano recém-criado
+        },
+      });
+    }
+  }
+
+  // const companyStruct = await db.company.create({
+  //   data: {
+  //     name: "Struct EJ",
+  //     email: "projetostrategis@gmail.com",
+  //     cnpj: "21.803.569/0001-65",
+  //     type: "Matriz",
+  //     phone: "(32) 3025-0102",
+  //     stateRegistration: "0771508800122",
+  //     taxRegime: "Simples Nacional",
+  //     address:
+  //       "Campus Universitario Darcy Ribeiro S/n Univ de Brasilia Edif Predio SG",
+  //     city: "Brasília",
+  //     neighborhood: "Asa Norte",
+  //     federativeUnit: "DF",
+  //     cep: "70910-900",
+  //     // legalResponsibleId: "",
+  //   },
+  // });
+  // await createUserWithRole({
+  //   name: "Leonardo Côrtes",
+  //   email: "leonardo.cortes@struct.unb.br",
+  //   phone: "(61) 99116-4633",
+  //   roleName: "Administrador",
+  //   companyId: companyStruct.id,
+  // });
+  // await createUserWithRole({
+  //   name: "Matheus das Neves Fernandes",
+  //   email: "matheusnf@struct.unb.br",
+  //   phone: "(61) 99999-9999",
+  //   roleName: "Administrador",
+  //   companyId: companyStruct.id,
+  // });
+  // await createUserWithRole({
+  //   name: "Guilherme Sampaio",
+  //   email: "guilherme.sampaio@struct.unb.br",
+  //   phone: "(61) 99999-9999",
+  //   roleName: "Administrador",
+  //   companyId: companyStruct.id,
+  // });
+  // await createUserWithRole({
+  //   name: "Willyan Marques",
+  //   email: "willyan.marques@struct.unb.br",
+  //   phone: "(61) 99999-9999",
+  //   roleName: "Administrador",
+  //   companyId: companyStruct.id,
+  // });
 }
 
 main().catch((e) => console.log(e));
